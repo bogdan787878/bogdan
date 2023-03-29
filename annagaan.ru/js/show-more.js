1290fa39d1d666f1544c2ca28c34ec15d87af7f0
bogdan787878 <129301338+bogdan787878@users.noreply.github.com>
2023-03-29 17:32:14 +0300

    Delete annagaan.ru directory

--- annagaan.ru/js/show-more.js
+++
@@ -1,9 +0,0 @@
- $(".card").each(function() {
-    let more = $(this).find(".show-more");
-    let hide = $(this).find(".hide-content");
-    hide.hide();
-    more.click(function() {
-        hide.slideToggle();
-        more.text(more.text() == "Скрыть" ? "Показать еще" : "Скрыть");
-    });
-});
